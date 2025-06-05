ae63aee89a9e850595d7ece6c1bf1bcec372fec4
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-06-05 22:35:00 +0530

    code added

---
+++ PSClasses/React/practice-react-app/src/components/InputFocus/InputFocus.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputFocus = () => {
+  return (
+    <div>
+        <input type='text' placeholder='type here...' />
+        <button>Focus Input</button>
+    </div>
+  )
+}
+
+export default InputFocus
