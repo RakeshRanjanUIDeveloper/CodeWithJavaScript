e3dbf0565c6d8a45d269f1c62632c835b5867c1b
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-07-06 00:56:37 +0530

    code added

--- NamasteDev/NamasteDev_ReactJS/revision-react-app/src/App.jsx
+++ NamasteDev/NamasteDev_ReactJS/revision-react-app/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import SearchApp from './components/SearchApp/SearchApp'
 import TodoApp from './components/TodoApp/TodoApp'
-
+import DebounceApp from './components/DebounceApp/DebounceApp'
 function App() {
   return (
     <>
-      <TodoApp />
+      {/* <TodoApp /> 
+      <SearchApp />*/}
+        <DebounceApp />
     </>
   )
 }
