dac5d80f0a94f94176214d7e9a44ddaff2a00ca3
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-07-13 23:08:07 +0530

    code added

---
+++ PSClasses/React/linkedin-questions-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import MyComponent from './components/Render/MyComponent'
+import MyComponentt from './components/Render/MyComponentt'
+
+function App() {
+  return (
+    <React.Fragment>
+      <MyComponent />
+      <MyComponentt />
+    </React.Fragment>
+  )
+}
+
+export default App
