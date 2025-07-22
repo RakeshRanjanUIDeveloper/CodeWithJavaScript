7c0a799b07e9e1161aeb6e84bab2754fb09719f3
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-07-23 00:08:16 +0530

    code added

---
+++ NeoG/practiceset-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Practiceset1 from './components/Practiceset1/Practiceset1'
+
+function App() {
+  return (
+    <React.Fragment>
+      <Practiceset1 />
+    </React.Fragment>
+  )
+}
+
+export default App
--- PSClasses/React/linkedin-questions-app/src/App.jsx
+++ PSClasses/React/linkedin-questions-app/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import MyComponent from './components/Render/MyComponent'
 import MyComponentt from './components/Render/MyComponentt'
+import MyComponenttt from './components/Render/MyComponenttt'
 
 function App() {
   return (
     <React.Fragment>
       <MyComponent />
       <MyComponentt />
+      <MyComponenttt />
     </React.Fragment>
   )
 }
