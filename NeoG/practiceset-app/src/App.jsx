1a78012b353db192578d6f141fc3d2beae18eed8
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-07-26 13:30:45 +0530

    code added

--- NeoG/practiceset-app/src/App.jsx
+++ NeoG/practiceset-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Practiceset1 from './components/Practiceset1/Practiceset1'
 import PracticeSet2 from './components/PracticeSet2/PracticeSet2'
+import PracticeSet3 from './components/PracticeSet3/PracticeSet3'
 
 function App() {
   return (
     <React.Fragment>
-     {/* <Practiceset1 /> */}
-      <PracticeSet2 />
+     {/* <Practiceset1 /> 
+      <PracticeSet2 />*/}
+      <PracticeSet3 />
     </React.Fragment>
   )
 }
