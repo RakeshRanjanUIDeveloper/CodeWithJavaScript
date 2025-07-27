9ccad2f4686d84cc66d3d587444c7aa82a6f6157
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-07-28 00:08:16 +0530

    code added

--- NeoG/practiceset-app/src/App.jsx
+++ NeoG/practiceset-app/src/App.jsx
@@ -3,14 +3,16 @@ import Practiceset1 from './components/Practiceset1/Practiceset1'
 import PracticeSet2 from './components/PracticeSet2/PracticeSet2'
 import PracticeSet3 from './components/PracticeSet3/PracticeSet3'
 import PracticeSet4 from './components/PracticeSet4/PracticeSet4'
+import PracticeSet5 from './components/PracticeSet5/PracticeSet5'
 
 function App() {
   return (
     <React.Fragment>
      {/* <Practiceset1 /> 
       <PracticeSet2 />
-      <PracticeSet3 />*/}
-      <PracticeSet4 />
+      <PracticeSet3 />
+      <PracticeSet4 />*/}
+      <PracticeSet5 />
     </React.Fragment>
   )
 }
---
+++ NeoG/practiceset-app/src/components/PracticeSet5/PracticeSet5.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UsersStatus from './UsersStatus'
+import Products from './Products'
+
+const PracticeSet5 = () => {
+  return (
+    <div>
+        <UsersStatus />
+        <Products />
+    </div>
+  )
+}
+
+export default PracticeSet5
