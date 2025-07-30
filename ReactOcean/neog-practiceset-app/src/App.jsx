4de8bd1632ddcdf3cb8c6b152caaac0b94bca44e
Rakesh Ranjan <rakeshmodi034@gmail.com>
2025-07-30 15:12:00 +0530

    code added

---
+++ ReactOcean/neog-practiceset-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Practiceset1 from './components/Practiceset1/Practiceset1'
+import PracticeSet2 from './components/PracticeSet2/PracticeSet2'
+
+function App() {
+  return (
+    <React.Fragment>
+     {/* <Practiceset1 /> */}
+      <PracticeSet2 />
+    </React.Fragment>
+  )
+}
+
+export default App
