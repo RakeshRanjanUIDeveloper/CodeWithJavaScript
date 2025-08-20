bde12b37d24bd881c91c872834ac77ae1c40f5ee
Rakesh Ranjan <rakeshmodi034@gmail.com>
2025-08-20 16:02:45 +0530

    code added

--- ReactOcean/neog-practiceset-app/src/components/PracticeSet6/PracticeSet6.jsx
+++ ReactOcean/neog-practiceset-app/src/components/PracticeSet6/PracticeSet6.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Products from './Products'
 import Todos from './Todos'
+import HabitTracker from './HabitTracker'
+import VideoLibrary from './VideoLibrary'
+import SocialMedia from './SocialMedia'
 
 const PracticeSet6 = () => {
   return (
     <div>
         <Products />
         <Todos />
+        <HabitTracker />
+        <VideoLibrary />
+        <SocialMedia />
     </div>
   )
 }
