72bee17cb905bf31062772762a96326be360fc00
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-09-20 22:00:46 +0530

    code added

--- ReactOcean/linkedin-questions-app/src/App.jsx
+++ ReactOcean/linkedin-questions-app/src/App.jsx
@@ -6,16 +6,31 @@ import MyComponenttt from './components/Render/MyComponenttt'
 import CounterState from './components/UseReducer/CounterState'
 import CounterReducer from './components/UseReducer/CounterReducer'
 import CounterObject from './components/UseReducer/CounterObject'
+import { Test } from './components/TestKnowledge/Test'
+import CleanUpFunction from './components/TestKnowledge/CleanUpFunction'
+import { LifeCycleDemo } from './components/TestKnowledge/LifeCycleDemo'
+import HOC from './components/TestKnowledge/HOC'
+import ThemeProvider from './components/TestKnowledge/ContextAPI/ThemeProvider'
+import ThemePage from './components/TestKnowledge/ContextAPI/ThemePage'
+import UseMemo from './components/TestKnowledge/UseMemo'
 
 function App() {
   return (
     <React.Fragment>
       {/* <MyComponent />
       <MyComponentt />
-      <MyComponenttt /> */}
+      <MyComponenttt /> 
       <CounterState />
       <CounterReducer />
       <CounterObject />
+        <Test />
+        <CleanUpFunction />
+        <LifeCycleDemo />
+        <HOC />
+        <ThemeProvider>
+          <ThemePage />
+        </ThemeProvider>*/}
+        <UseMemo />
     </React.Fragment>
   )
 }
