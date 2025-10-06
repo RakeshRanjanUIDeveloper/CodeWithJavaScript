1250ace65dd251fb4f29d205e30a78f3596377f8
Rakesh Ranjan <rakeshmodi034@gmail.com>
2025-10-06 16:57:52 +0530

    code added

--- ReactOcean/testing-app/src/__tests__/GetByText.test.js
+++ ReactOcean/testing-app/src/__tests__/GetByText.test.js
@@ -3,7 +3,10 @@ import GetByText from "../components/GetByText"
 
 test("single button testing", ()=>{
     render(<GetByText />)
-    const button = screen.getByText('Login');
+    const button = screen.getByText('Login', {exact:false});
     expect(button).toBeInTheDocument()
     expect(button).toHaveClass('mybtn');
+    expect(button).not.toHaveClass('s');
+    const div = screen.getByText((content, element) => content.startsWith("Hello"));
+    expect(div).toBeInTheDocument();
 })
--- ReactOcean/testing-app/src/components/GetByText.js
+++ ReactOcean/testing-app/src/components/GetByText.js
@@ -4,6 +4,7 @@ const GetByText = () => {
   return (
     <div>
         <button className='mybtn'>Login</button>
+        <div className='test'>Hello World</div>
     </div>
   )
 }
